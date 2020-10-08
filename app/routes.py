12e55f53173f08040ec9a2c0b3e336289e490fcf
IlliaVokhranov <vokhranov@gmail.com>
2020-10-09 00:36:58 +0300

    just an experiment

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template
 from app.models import TodoItem
+from app import db
 
-@app.route("/")
+@app.route('/')
 def home():
     incomplete = TodoItem.query.filter_by(complete=False).all()
     complete = TodoItem.query.filter_by(complete=True).all()
 
     return render_template('index.html', incomplete=incomplete, complete=complete)
+
+@app.route('/init')
+def init():
+    db.create_all()
