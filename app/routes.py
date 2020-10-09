eca67896765ee2ca17affe9fe5d5e198754480ce
IlliaVokhranov <vokhranov@gmail.com>
2020-10-09 19:41:27 +0300

    routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app.models import TodoItem
+from app import db
 
 @app.route('/')
 def home():
@@ -8,3 +9,21 @@ def home():
     complete = TodoItem.query.filter_by(complete=True).all()
 
     return render_template('index.html', incomplete=incomplete, complete=complete)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = TodoItem(text=request.form['todoitem'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+
+@app.route('/complete/<id>')
+def complete(id):
+    todo = TodoItem.query.filter_by(id=int(id)).first()
+    todo.complete = True
+    db.session.commit()
+
+    return redirect(url_for('home'))
