57bac3c8483dfeeb6b71271dc88adb942c11053c
IlliaVokhranov <vokhranov@gmail.com>
2020-10-09 19:32:36 +0300

    typo

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,10 @@
 from app import app
 from flask import render_template
 from app.models import TodoItem
-from app import db
 
 @app.route('/')
 def home():
     incomplete = TodoItem.query.filter_by(complete=False).all()
     complete = TodoItem.query.filter_by(complete=True).all()
 
-    return render_template('index.html', incomplete=incomplete, complete=complete)gi
+    return render_template('index.html', incomplete=incomplete, complete=complete)
