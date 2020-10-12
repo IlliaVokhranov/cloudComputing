d0dfe360202e26da2ab52475eac9d15e0ce5c81b
IlliaVokhranov <vokhranov@gmail.com>
2020-10-12 13:39:07 +0300

    static folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+app.static_folder = 'static'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
