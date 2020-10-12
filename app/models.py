1a75dc198905e7e39de0821ff0345573904dbd13
IlliaVokhranov <vokhranov@gmail.com>
2020-10-12 12:28:32 +0300

    refactoring

--- app/models.py
+++ app/models.py
@@ -1,10 +1,6 @@
 from app import db
 
-
 class TodoItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return self.text
